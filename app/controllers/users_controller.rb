c03cb4da41250d2aabc940be5cde0f24c4b9d0e5
suzukisan22 <getty2017@gmail.com>
2016-09-28 01:52:21 +0000

    login  logout coded!

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,13 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+  end
+  
   private
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
