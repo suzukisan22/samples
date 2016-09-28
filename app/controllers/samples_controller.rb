58b6ccf30b6a0673a985be415db6336513420823
suzukisan22 <getty2017@gmail.com>
2016-09-28 04:34:57 +0000

    add posting samples

--- app/controllers/samples_controller.rb
+++ app/controllers/samples_controller.rb
@@ -1,5 +1,5 @@
 class SamplesController < ApplicationController
-    before_action :logged_in_user, only: [:create]
+    before_action :logged_in_user, only: [:create, :destroy]
     
     def create
       @sample = current_user.samples.build(samples_params)
@@ -9,6 +9,14 @@ class SamplesController < ApplicationController
       else 
         render 'static_pages/home'
       end
+    end
+    
+    def destroy
+      @sample = current_user.samples.find_by(id: params[:id])
+      return redirect_to root_url if @sample.nil?
+      @sample.destroy
+      flash[:success] = "Tweet deleted"
+      redirect_to request.referer || root_url
     end
     
     private
